eb9fb32a71ea4ea6b4e7f721f0d32c1abbb3595a
Zcy. <157526994+Zyneeee@users.noreply.github.com>
2024-07-16 21:53:17 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+import json
+import random
+
+app = Flask(__name__)
+
+def load_quotes():
+    with open('quotes.json', 'r') as file:
+        return json.load(file)
+
+quotes = load_quotes()
+
+@app.route('/api/quote', methods=['GET'])
+def get_random_quote():
+    return jsonify(random.choice(quotes))
+
+if __name__ == '__main__':
+    app.run(debug=True)
